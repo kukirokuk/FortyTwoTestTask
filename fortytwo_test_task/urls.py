ba04769db3a49ddf6cad64ddc4bcbe27ad5baceb
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2016-02-07 12:52:15 +0200

    ticket:1 url fixed

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns(
