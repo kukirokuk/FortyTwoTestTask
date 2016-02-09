bf8210a2d23d8350ce32fa1259d36ae36666eb3d
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2016-02-09 11:08:58 +0200

    tests and template added

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -135,3 +135,146 @@ class ContactModelTest(TestCase):
         self.assertEqual(saved_person.last_name, 'Shchur')
         self.assertEqual(person_from_fixtures.name, 'Oleksandr')
         self.assertEqual(person_from_fixtures.last_name, 'Vinnichuk')
+
+
+class ContactEditFormTest(TestCase):
+
+    def setUp(self):
+        # Contact.objects.all().delete()
+        self.url = reverse('user_detail')
+        self.data = {
+            'name': 'Pablo',
+            'last_name': 'Escobar',
+            'date_of_birth': '1949-12-01',
+            'skype': 'pablito',
+            'jabber': 'pablito@khavr.com',
+            'email': 'pablo.e@gmail.com',
+            'other_contacts': 'mobile +79023405442',
+            'bio': 'Born in Antioquia, Colombia'}
+
+    def test_form_fields(self):
+        '''
+         Check form
+        '''
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
+
+        self.response = self.client.get(self.url)
+
+        # check response status
+        self.assertEqual(self.response.status_code, 200)
+
+        # check form fields
+        self.assertIn('name="name"', self.response.content)
+        self.assertIn('name="last_name"', self.response.content)
+        self.assertIn('name="date_of_birth"', self.response.content)
+        self.assertIn('name="email"', self.response.content)
+        self.assertIn('name="jabber"', self.response.content)
+        self.assertIn('name="other_contacts"', self.response.content)
+        self.assertIn('name="bio"', self.response.content)
+        self.assertIn('name="image"', self.response.content)
+        self.assertIn('action="%s"' % self.url,
+                      self.response.content)
+
+    def test_post(self):
+        '''
+         Check form saves data
+        '''
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
+
+        # check response status
+        self.response = self.client.post(self.url, self.data)
+        self.assertEqual(self.response.status_code, 200)
+
+        # test updated model saved data from post
+        person = Contact.objects.get(pk=1)
+        self.assertEqual(person.name, self.data['name'])
+        self.assertEqual(person.last_name, self.data['last_name'])
+
+    def test_auth(self):
+        '''
+         Check authentication works properly
+        '''
+        # access as anonymous user
+        response = self.client.get(self.url, follow=True)
+
+        # check that we are on login form
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Login for profile data edit', response.content)
+
+    def test_ajax(self):
+        '''
+         Check ajax
+        '''
+        # Contact.objects.all().delete()
+
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
+
+        # check ajax get request
+        kwargs = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
+        response = self.client.get(reverse('user_detail'), **kwargs)
+        self.assertEquals(response.status_code, 200)
+
+        # check ajax valid post request with all required fields filled
+        response = self.client.post(reverse('user_detail'),
+                                    self.data, **kwargs)
+        self.assertEquals(response.status_code, 200)
+        person = Contact.objects.get(pk=1)
+        self.assertEqual(person.name, self.data['name'])
+        self.assertEqual(person.last_name, self.data['last_name'])
+
+        # check invalid post request with missed ruqeired fields
+        response = self.client.post(reverse('user_detail'),
+                                    {'name': 'Juan',
+                                    'last_name': ''}, **kwargs)
+        self.assertEquals(response.status_code, 200)
+        person = Contact.objects.get(pk=1)
+        self.assertNotEquals(person.name, 'Juan')
+
+    def test_valid_invalid_form(self):
+        '''
+         Check form validation
+        '''
+
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
+
+        # test valid form with all required fields added
+        person = Contact.objects.create(
+            name=self.data['name'],
+            last_name=self.data['last_name'],
+            jabber=self.data['jabber'],
+            skype=self.data['skype'])
+        form = ContactForm(data={
+            'name': person.name,
+            'last_name': person.last_name,
+            'jabber': person.jabber,
+            'skype': person.skype})
+        self.assertTrue(form.is_valid())
+
+        Contact.objects.all().delete()
+
+        # test valid form with one required fields missed
+        person = Contact.objects.create(
+            name=self.data['name'],
+            last_name=self.data['last_name'],
+            jabber=self.data['jabber'])
+        form = ContactForm(data={
+            'name': person.name,
+            'last_name': person.last_name,
+            'jabber': person.jabber})
+        self.assertFalse(form.is_valid())
+
+    def test_wiget(self):
+        '''
+         Check widget appear at contact edit page
+        '''
+
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
+
+        # test widget appear at form page
+        response = self.client.get(reverse('user_detail'))
+        self.assertIn('<a href="/">Cancel</a>', response.content)
