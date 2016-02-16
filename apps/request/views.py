b31b131ae2aec9aa4eda147815dc67f461c6e936
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2016-02-16 17:59:01 +0200

    ticket:13 template and view fixed for priority sorting, css fixed, context processor added

--- apps/request/views.py
+++ apps/request/views.py
@@ -8,19 +8,24 @@ def request_store(request):
     Requests store and sorting
     '''
     order_by = request.GET.get('order_by', '')
-    # sort by priority which is passed in GET request
-    if order_by:
+    saved_requests = SavedRequest.objects.all()
+    saved_requests = saved_requests.exclude(priority=0)
+    count = SavedRequest.objects.count()
+    try:
+        if "all" in order_by:
+            saved_requests = saved_requests.order_by('-priority')
+            if request.GET.get('reverse', '') == '1':
+                saved_requests = saved_requests.reverse()
+            last_ten = saved_requests[:10]
+
+            return render(request, "show_requests.html", {'requests': last_ten,
+                          'title': count})
+        order_by = order_by or 1
         saved_requests = SavedRequest.objects.filter(priority=order_by)
-        saved_requests = saved_requests.exclude(priority=0)
         saved_requests = saved_requests.order_by('-id')
         count = len(saved_requests)
         last_ten = saved_requests[:10]
         return render(request, "show_requests.html", {'requests': last_ten,
                       'title': count})
-    # sort by standard selection priority=1
-    saved_requests = SavedRequest.objects.filter(priority=1).order_by('-id')
-    count = SavedRequest.objects.count()
-    last_ten = saved_requests[:10]
-
-    return render(request, "show_requests.html", {'requests': last_ten,
-                  'title': count})
+    except:
+        return render(request, "show_requests.html")
